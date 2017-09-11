d1e1efe8ca5b8e77e08e7cd16f899dad5e97d0d1
Seong Lee <selee@chapman.edu>
2017-09-11 06:50:26 +0000

    append Temporary file XXX  inside files

--- createfiles.sh
+++ createfiles.sh
@@ -1,6 +1,12 @@
 mkdir -p  tmpfiles
 
-touch tmpfiles/"Temporary File "file{001..100}.tmp
+touch tmpfiles/file{001..100}.tmp
+
+file_number=1
+while [  $file_number -lt 101 ]; do
+ echo "Temporary file $(printf "%03d" $file_number)"  >> tmpfiles/file$(printf "%03d" $file_number).tmp
+ file_number=$(( $file_number+1 ))
+done
 
 
 
